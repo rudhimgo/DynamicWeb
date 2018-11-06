209d749d8a5660af89959d9c84d55e14fb59343f
rudhigokhale <rudhigokhale@10-18-23-111.dynapool.wireless.nyu.edu>
2018-11-06 12:02:13 -0500

    added api express app

---
+++ apiexpressapp/README.md
@@ -0,0 +1,18 @@
+Hello! This is an express app that utilizes the New York Times API in which you can input a category/section for the New York Times (such as politics, world, home, etc.) and then recieve the titles/urls for all the top stories under that category. 
+
+In order to run this app, follow these steps:
+
+1) Download file to computer.
+
+2) cd into the folder in terminal and locally install express, ejs, body parser, and request, using npm init and then npm install.
+
+3) Go to https://developer.nytimes.com/ and generate your own API key.
+
+4) Replace the API key in the app.js file.
+
+5) Go back into the terminal, and in the folder run the command "node app.js"
+
+6) In your web browser, go to the web address localhost:3000 
+
+You are now running the app! 
+
---
+++ apiexpressapp/app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+
+var request = require('request');
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('index')
+	 let story = JSON.parse(body);
+})
+
+
+app.post('/', function(req, res){
+	let subsection = req.body.subsection;
+	let url = "https://api.nytimes.com/svc/topstories/v2/.q=${results.subsection}json"
+	request.get({
+  url: "https://api.nytimes.com/svc/topstories/v2/q=${results.subsection}.json",
+  qs: {
+    'api-key': "1f0b2087b98240409e52d724ebfc2673"
+  },
+}, function(err, response, body) {
+  //let story = JSON.parse(body);
+  console.log(body);
+})
+	
+request(url, function(err, response, body){
+		if(err){
+			res.render('index', {story: null, error: 'Error, try again'});
+		} else {
+			let story = JSON.parse(body)
+			console.log(story);
+			if(story.main == undefined){
+				res.render('index', {story: null, error: 'Error, try again'})
+			} else {
+				let storyText =` ${results.title} : ${results.url}`;
+				res.render('index', {story: storyText, error: null});
+			}
+		}
+	});
+
+});
+
+
+
+
+
+app.listen(3000, function(){
+	console.log('app is running of port 3000!')
+})
+
+
+
+
+
+
+
+
+
